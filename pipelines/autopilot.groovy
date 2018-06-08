5843d740684c080e16f0d377a668befb648ca889
Vincent YT Chang <vincent_yt_chang@trend.com.tw>
2018-06-08 17:01:01 +0800

    testmaster

--- pipelines/autopilot.groovy
+++ pipelines/autopilot.groovy
@@ -34,10 +34,10 @@ node() {
     def currentPath = "${env.WORKSPACE}\\pipelines\\autopilot_params\\${currentHour}"
 	echo "${currentPath}"
 	
-	checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'test/dynamic_pipeline_in_Lift']], 
+	checkout changelog: false, poll: false, scm: [$class: 'GitSCM', branches: [[name: 'master']], 
              doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], 
              userRemoteConfigs: [[credentialsId: "b6daa83e-1669-4908-baee-554f27a49a40", 
-             refspec: '', url: 'https://github.com/logikeer/Testing.git']]]
+             refspec: '', url: 'git@github.com:logikeer/Testing.git']]]
 
     //def fileList = []
     //(currentPath as File).eachFile groovy.io.FileType.FILES, {
